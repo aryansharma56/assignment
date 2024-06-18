005d4c6429caa897131256fbe8ebd316ce6f2626
greedybrute <asharma1_be20@thapar.edu>
2024-06-18 10:39:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [val, setval] = useState(0);
+  const [boxesToDisplay, setBoxesToDisplay] = useState([]);
+  function handleChange(event) {
+    const inp = event.target.value;
+    setval(inp);
+    console.log(val);
+    console.log(typeof val);
+    // if (!Number.isNaN(parseInt(inp))) {
+    //   window.alert("invalid input");
+    // }
+  }
+
+  const renderBoxes = () => {
+    const boxes = [];
+    for (let i = 1; i <= val; i++) {
+      const boxStyle = {
+        backgroundColor: "blue",
+        textAlign: "center",
+        padding: "20px",
+        margin: "10px",
+        display: "inline-block",
+      };
+
+      boxes.push(
+        <div key={i} style={boxStyle}>
+          <p style={{ margin: 0 }}>box {i}</p>
+        </div>
+      );
+      setBoxesToDisplay(boxes);
+    }
+    return boxes;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Enter the number of boxes</h1>
+        <input onChange={handleChange}></input>
+        <button onClick={renderBoxes}>Submit</button>
+        <div>{boxesToDisplay}</div>
       </header>
     </div>
   );
